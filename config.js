4c9eb69109a51a6f2c27b28c5909a67717c050c3
Pahanmax767 <125570263+Pahanmax767@users.noreply.github.com>
2023-05-13 11:45:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['94701469704', 'PAHAN MAX', true],
+  ['94701469704', 'Ash', true], 
   [''] 
 ] //Number of owners
 
